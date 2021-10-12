47d6fa9493e9e1c259b453460a18b7480680baf3
igorlizyakin <igor.lizyakin@gmail.com>
2021-10-12 13:16:39 +0300

    исправление работы

--- cal1.sh
+++ cal1.sh
@@ -1,34 +1,40 @@
 #!/bin/bash
 echo "Старт Колькулятора"
-cat file1.txt | while read y
-do
-echo "line 1: $y "
-#done1
-a=$((y))
-echo $a
-done
+#cat $! #file1.txt
+# | while read $1
+#do
+#echo "line 1: "$1 
+#done
+#a=$(($1))
+#echo $a
+#done
 #echo $?
-cat file2.txt | while read x
-do
-echo "line 2: $x "
+#cat file2.txt | while read x
+#do
+#echo "line 2:" $2 "
 #done2
-b=$((x))
-echo $b
-done
-
-if [ $a -ge $b ]
+#b=$((x))
+#echo $b
+#done
+line1=$(cat $1)
+result1=$(($line1))
+echo $result1
+line2=$(cat $2)
+result2=$(($line2))
+echo $result2
+if [ $result1 -ge $result2 ]
 then
-echo "Значение:"
+echo "Значение:"$result1
 echo "line1 больше или равен line2"
 else
-echo "Значение:"
+echo "Значение:"$result2
 echo "line2 больше или равен line1"
 fi
 
 
-if [ $a -eq $b ]
+if [ $result1 -eq $result2 ]
 then
-echo "line1 не равен line2"
+echo "line1  равен line2"
 else
-echo "line2 равен line1"
+echo "line2 не равен line1"
 fi
